81078f18226526b78c58b7101e22f61393faab95
Reas Vyn <reasnov.app@gmail.com>
2026-02-11 19:09:19 +0700

    chore: ensure GitHub Pages compatibility and add deploy script

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html',
+			fallback: '404.html', // Better for GitHub Pages SPA
 			pages: 'build',
 			assets: 'build',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/rupa-pixel-editor' : ''
+		}
 	}
 };
 
